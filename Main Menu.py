289af848a4d01f41db9c9513657bee0af91da400
aaronbauer <masterpug5red@gmail.com>
2024-07-13 23:22:07 -0400

    test commit

--- Main Menu.py
+++ Main Menu.py
@@ -2,16 +2,28 @@ import pygame
 import sys
 import random
 import time
+#hello caffeine Rushers
 
-
+# hi
 def main():
     # turn on pygame
     pygame.init()
+    screen_width = 1000 #defines the width
+    screen_height = 600 #defines the height
 #hi
     # create a screen
-    pygame.display.set_caption("Cool Project")
+    pygame.display.set_caption("Caffeine Rush")
     # TODO: Change the size of the screen as you see fit!
-    screen = pygame.display.set_mode((640, 480))
+    screen = pygame.display.set_mode((screen_width, screen_height)) #sets the screen dimentions
+    font = pygame.font.Font(None, 100) #creates the font
+    font2 = pygame.font.Font(None, 150)
+    text_surface = font.render("PLAY", True, (0, 0, 0)) #creates text surface
+    text_surfaceTitle = font2.render("Caffeine Rush", True, (0,0,0))
+    text_rect = text_surface.get_rect() #get the rect of the play button
+    text_rectTitle = text_surfaceTitle.get_rect()
+    text_rect.center = (screen_width // 2, screen_height // 2) #centers text
+    text_rectTitle = (screen_width - 860, screen_height - 500)
+
 
     # let's set the framerate
     clock = pygame.time.Clock()
@@ -25,6 +37,8 @@ def main():
 
         # TODO: Fill the screen with whatever background color you like!
         screen.fill((255, 67, 233))
+        screen.blit(text_surface, text_rect) #"Play"
+        screen.blit(text_surfaceTitle, text_rectTitle)
 
         # TODO: Add your project code
 
