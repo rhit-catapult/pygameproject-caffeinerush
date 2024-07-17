6e40a28e00f55faa5291a76082aaa26f923ac287
aaronbauer <masterpug5red@gmail.com>
2024-07-17 15:23:55 -0400

    test commit

--- Task3.py
+++ Task3.py
@@ -53,6 +53,7 @@ def main():
     pygame.display.set_caption('Winner Screen')
     screen.fill(pygame.Color('gray'))
     clock = pygame.time.Clock()
+
     counter = 0
     image2 = pygame.image.load("Spacebarbackground.jpeg")
     image2 = pygame.transform.scale(image2, (1000, 600))
@@ -61,6 +62,12 @@ def main():
     text_surface = font.render("Match Order To Customer", True, (25, 150, 20))  # creates text surface
     text_rect = text_surface.get_rect()  # get the rect of the play button
     text_rect.center = (500, 40)  # centers text
+    text_color = (0, 0, 0)
+    countdown_seconds = 6
+    start_time = pygame.time.get_ticks()  # Get initial time in milliseconds
+    timer_font = pygame.font.Font(None, 100)
+    timer_text = ""
+    task_font = pygame.font.Font('papyrus.ttf', 50)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -71,35 +78,57 @@ def main():
                    mix.show_left = not mix.show_left
                 counter = counter + 1
                 print("space_count =" + str(counter))
-            if counter > 30:
-                font = pygame.font.Font("calibri-italic.ttf", 100)
-                text_surface = font.render("!YOU WIN!", True, (25, 150, 20))  # creates text surface
-                while True:
-                    for event in pygame.event.get():
-                        if event.type == pygame.QUIT:
-                            sys.exit()
-                    clock.tick(60)
-                    screen.fill("light Green")
-                    text_rect = text_surface.get_rect()  # get the rect of the play button
-                    text_rect.center = (300, 40)  # centers text
-                    screen.blit(text_surface, text_rect.center)
-                    task_text2 = "NEXT!"
-                    task_text_surface2 = font.render(task_text2, True, (0, 0, 0))
-                    task_text_rect2 = task_text_surface2.get_rect(center=(500, 300))
-                    screen.blit(task_text_surface2, task_text_rect2)
-                    if event.type == pygame.MOUSEBUTTONDOWN:
-                        if task_text_rect2.collidepoint(event.pos):
-                            pygame.quit()
-                            Task2.main()
-                    pygame.display.update()
-        clock.tick(60)
+        if counter > 30:
+            font = pygame.font.Font("calibri-italic.ttf", 100)
+            text_surface = font.render("!YOU WIN!", True, (25, 150, 20))  # creates text surface
+            while True:
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        sys.exit()
+
+                clock.tick(60)
+
+                screen.fill(pygame.Color('lightblue'))
+                task_text = "Task completed!"
+                screen.blit(image2, (0, 0))
+                screen.blit(text_surface, (50, 50))
+                pygame.display.update()
+
+        elapsed_time = pygame.time.get_ticks() - start_time
+        remaining_time = max(0, countdown_seconds * 1000 - elapsed_time)
+        seconds = remaining_time // 1000
+
+        seconds = seconds % 60
+        timer_text = f"{seconds:7}"
+
+        screen.fill(pygame.Color('lightblue'))
+        task_text = "Task completed!"
+        screen.blit(image2, (0, 0))
+
+        timer_surface = timer_font.render(timer_text, True, text_color)
+        timer_rect = timer_surface.get_rect(center=(5, 50))
+        screen.blit(timer_surface, timer_rect)
+        if remaining_time < 1:
+            screen.fill(pygame.Color('red'))
+            task_text6 = "Task failed!"
+            task_text7 = "RETRY!"
+            task_text_surface6 = task_font.render(task_text6, True, (0, 0, 0))
+            task_text_rect6 = task_text_surface6.get_rect(center=(500, 200))
+            task_text_surface7 = task_font.render(task_text7, True, (0, 0, 0))
+            task_text_rect7 = task_text_surface7.get_rect(center=(500, 300))
+            screen.blit(task_text_surface7, task_text_rect7)
+            screen.blit(task_text_surface6, task_text_rect6)
+            task_rec = task_text_surface7.get_rect()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if task_text_rect7.collidepoint(event.pos):
+                    pygame.quit()
+                    MainMenu.main()
+
+        # pygame.display.update()
+
+
+
         # TODO 15: Make a Hero, named alyssa, with appropriate images, starting at position x=700 y=400.
-
-
-        if True:  # task_screen = pygame.display.set_mode((1000, 600))
-            screen.fill(pygame.Color('lightblue'))
-            task_text = "Task completed!"
-            screen.blit(image2, (0, 0))
 
         mix.draw()
         pygame.display.update()
